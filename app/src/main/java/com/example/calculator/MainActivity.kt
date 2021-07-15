e6d30ef72e4f7cfa35621b49c2bd87651c50dda8
Calrissian <va2005art@gmail.com>
2021-07-15 14:01:08 +0300

    Update Logic

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -9,28 +9,25 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.cardview.widget.CardView
 import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import net.objecthunter.exp4j.Expression
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
     var nowSysButton: TextView? = null
 
-    lateinit var textForNums: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textForNums = main_text
         switchThemeMode(!getSharedPreferences("0", 0).getBoolean("night_mode", false))
+        
         val numClick = View.OnClickListener{
             addNewChar((it as TextView).text.toString())
         }
 
         val sysClick = View.OnClickListener {
             addNewChar((it as TextView).text.toString())
-            if (nowSysButton != null){
-                nowSysButton!!.setBackgroundColor(getColor(R.color.colorBackItem))
-            }
+            toNormalNowButton()
             nowSysButton = it
             nowSysButton!!.setBackgroundColor(getColor(R.color.colorAccentButton))
         }
@@ -44,24 +41,24 @@ class MainActivity : AppCompatActivity() {
         c.setOnClickListener {
             main_text.text = ""
             second_text.text = ""
-            textForNums = main_text
+            toNormalNowButton()
         }
 
         card_so.setOnClickListener {
             try {
-                val match = ExpressionBuilder(textForNums.text.toString()).build()
-                val result = match.evaluate()
-                if (second_text.text.toString().isEmpty()){
-                    second_text.text = textForNums.text.toString()
-                    textForNums = second_text
+                val match = if (main_text.text.isNotEmpty()) {
+                    ExpressionBuilder(main_text.text.toString()).build()
+                }else {
+                    ExpressionBuilder(second_text.text.toString()).build()
                 }
+                val result = match!!.evaluate()
+                second_text.text = main_text.text.toString()
                 if (".0" in result.toString()){
                     main_text.text = result.toLong().toString()
                 }else{
                     main_text.text = result.toString()
                 }
-                if (nowSysButton != null)
-                    nowSysButton!!.setBackgroundColor(getColor(R.color.colorBackItem))
+                toNormalNowButton()
             }catch (e: Exception){
                 Snackbar.make(so, "Ошибка!\n${e.message}", Snackbar.LENGTH_SHORT).show()
             }
@@ -73,19 +70,24 @@ class MainActivity : AppCompatActivity() {
 
         delete_num.setOnClickListener {
             try {
-                val delete_char = textForNums.text.toString().last().toString()
-                textForNums.text =
-                    textForNums.text.toString().substring(0, textForNums.text.toString().length - 1)
-                if (delete_char in arrayListOf("+", "-", "/", "*") && nowSysButton != null)
-                    nowSysButton!!.setBackgroundColor(getColor(R.color.colorBackItem))
+                val delete_char = main_text.text.toString().last().toString()
+                main_text.text =
+                    main_text.text.toString().substring(0, main_text.text.toString().length - 1)
+                if (delete_char in arrayListOf("+", "-", "/", "*"))
+                    toNormalNowButton()
             }catch (e: Exception){
 
             }
         }
     }
 
+    private fun toNormalNowButton(){
+        if (nowSysButton != null)
+            nowSysButton!!.setBackgroundColor(getColor(R.color.colorBackItem))
+    }
+
     private fun addNewChar(char: String){
-        textForNums.append(char)
+        main_text.append(char)
     }
 
     private fun switchThemeMode(now_theme_night: Boolean = resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK == Configuration.UI_MODE_NIGHT_YES){
